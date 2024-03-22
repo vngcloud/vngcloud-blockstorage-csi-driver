0b10580e11a635ecf9b06d4a709dba7a339551ab
Cuong. Duong Manh <cuongpigerr@gmail.com>
2024-03-22 15:30:28 +0700

    [feat] identity server

---
+++ pkg/driver/identity.go
@@ -0,0 +1,47 @@
+package driver
+
+import (
+	"context"
+
+	"github.com/container-storage-interface/spec/lib/go/csi"
+	"k8s.io/klog/v2"
+)
+
+func (d *Driver) GetPluginInfo(ctx context.Context, req *csi.GetPluginInfoRequest) (*csi.GetPluginInfoResponse, error) {
+	klog.V(6).InfoS("GetPluginInfo: called", "args", *req)
+	resp := &csi.GetPluginInfoResponse{
+		Name:          DriverName,
+		VendorVersion: driverVersion,
+	}
+
+	return resp, nil
+}
+
+func (d *Driver) GetPluginCapabilities(ctx context.Context, req *csi.GetPluginCapabilitiesRequest) (*csi.GetPluginCapabilitiesResponse, error) {
+	klog.V(6).InfoS("GetPluginCapabilities: called", "args", *req)
+	resp := &csi.GetPluginCapabilitiesResponse{
+		Capabilities: []*csi.PluginCapability{
+			{
+				Type: &csi.PluginCapability_Service_{
+					Service: &csi.PluginCapability_Service{
+						Type: csi.PluginCapability_Service_CONTROLLER_SERVICE,
+					},
+				},
+			},
+			{
+				Type: &csi.PluginCapability_Service_{
+					Service: &csi.PluginCapability_Service{
+						Type: csi.PluginCapability_Service_VOLUME_ACCESSIBILITY_CONSTRAINTS,
+					},
+				},
+			},
+		},
+	}
+
+	return resp, nil
+}
+
+func (d *Driver) Probe(ctx context.Context, req *csi.ProbeRequest) (*csi.ProbeResponse, error) {
+	klog.V(6).InfoS("Probe: called", "args", *req)
+	return &csi.ProbeResponse{}, nil
+}
